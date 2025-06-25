574bdd32152125b46f21caa446c991c2ff9d7e8a
Irshad-dude <mdirshadalam2511@gamil.com>
2025-06-25 15:52:56 +0530

    Updated with new file

---
+++ Asignement/Array.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Array 
+{
+    
+    public static void main(String[] args) {
+        int sum=0;
+        Scanner sc = new Scanner(System.in);
+        int [] arr =new int [5];
+        System.out.print("Enter the array element : ");     
+        for(int i=0;i<5;i++)
+            {
+                arr[i] = sc.nextInt();
+            }    
+        for(int i=0; i<5;i++)
+            {
+               sum +=arr[i];
+            }
+        System.out.println("This is the sum of all element : " + sum);     // This is the sum of array
+        int avg =sum/5;
+        System.out.println("This is the avg of all element : " + avg);     // This is the avg of array
+        
+        int num =arr[0];
+        for(int i=0;i<4;i++)
+        {
+            if(num<arr[i+1])
+            {
+                num=arr[i+1];
+            }  
+        }
+        System.out.print("this is the largest element in array : "+num);   //This is the larger no array                                       
+    }
+}
+    
